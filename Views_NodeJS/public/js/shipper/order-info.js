83159609a388b1ce7cfe827995f427483b979084
unknown <hokhacminhtri@gmail.com>
2021-12-31 12:40:27 +0700

    View: Add feature Get order detail info

---
+++ Views_NodeJS/public/js/shipper/order-info.js
@@ -0,0 +1,7 @@
+// function onPageItemClick(page) {
+//   location.href = `/shipper/order-info?page=${page}`;
+// }
+
+// $(document).ready(function () {
+//   pagination("#pagination", total, pageSize, page, onPageItemClick);
+// });
