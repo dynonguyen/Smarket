8fb459d521822f0163c76e653b0ca0ec1cb76de8
XuanBao <pxbao.itus@gmail.com>
2022-01-01 13:10:13 +0700

    Add Swagger for dotnet

---
+++ API_DOTNETCore/API_.NET/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using API_.NET.DAO;
+using API_.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API_.NET.Controllers
+{
+    [Route("api/[controller]/customer")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<Product> GetAllProductForCustomer()
+        {
+            return DAO_Product.GetAllProduct();
+        }
+    }
+}
