997e4d40d28a6813b16c412b5405d7b68dd9c47f
XuanBao <pxbao.itus@gmail.com>
2022-01-07 10:41:52 +0700

    dotnet api for customer feedback

--- API_DOTNETCore/API_.NET/Controllers/Store/ProductController.cs
+++ API_DOTNETCore/API_.NET/Controllers/Store/ProductController.cs
@@ -11,36 +11,6 @@ namespace API_.NET.Controllers.Store
     [ApiController]
     public class ProductController : ControllerBase
     {
-        // GET: api/Product
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Product/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Product
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Product/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
---
+++ API_DOTNETCore/API_.NET/DAO/Customer/DAO_Feedback.cs
@@ -0,0 +1,43 @@
+﻿using API_.NET.Models;
+using API_.NET.Utils;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API_.NET.DAO.Customer
+{
+    public class DAO_Feedback
+    {
+        // Set feedback for product
+        public static bool CustomerFeedback(OrderDetailFeedback fb)
+        {
+            try
+            {
+                using(var context = new SmarketContext())
+                {
+                    context.OrderDetailFeedback.Add(fb);
+                    context.SaveChanges();
+                    return true;
+                }
+            } catch
+            {
+                return false;
+            }
+        }
+        
+        // Get all feedback of a product
+        public static List<OrderDetailFeedback> GetFeedbacksOfProduct(int productId)
+        {
+            try
+            {
+                using(var context = new SmarketContext())
+                {
+                    return context.OrderDetailFeedback.FromSql(Utils_Queries.GetFeedbackOfProduct(productId)).ToList();
+                }
+            } catch
+            {
+                return null;
+            }
+        }
+    }
+}
